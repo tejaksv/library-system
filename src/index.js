8d62d489fdf6b43d837864a357d56b431f5b4ba5
Teja KSV <kattasaiteja09@gmail.com>
2025-02-07 16:44:09 +0530

    Table component with logics

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import HelloWorld from './HelloWorld';
 import reportWebVitals from './reportWebVitals';
+import TableRender from './TableRender';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelloWorld />
+    <TableRender />
   </React.StrictMode>
 );
 
