801fc9e85cf795e88c460578c4b5e33801909c5e
Tharak1221 <tarakjetipiti@gmail.com>
2025-02-17 17:24:15 +0530

    my changes

--- src/components/select.jsx
+++ src/components/select.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 
-function Select(props) {
-    const { title, id, options, value, setChangeValue } = props;
-
-    const onCommonChange = (e) => {
-        setChangeValue(e.target.value);
-    };
-    
+function Select({ title, options, value, setValue, id, name }) {
     return (
-        <div className="row" style={{ marginBottom: "5px" }}>
-            <label htmlFor={id} className="col-6">{title}</label>
-            <select id="country" name="country" className="col-5" onChange={onCommonChange}>
-                <option value="uk">UK</option>
-                <option value="usa">USA</option>
-                <option value="india">India</option>
-                <option value="australia">Australia</option>
-            </select>
+        <div className="row">
+            <label className="col-6">{title}</label>
+            <span className="col-5">
+                <select id={id} name={name} value={value} onChange={(e) => setValue(e.target.value)}>
+                    <option value="">Select {title}</option> 
+                    {options.map((option) => (
+                        <option key={option.value} value={option.value}>
+                            {option.label}
+                        </option>
+                    ))}
+                </select>
+            </span>
         </div>
-    )
+    );
 }
 
 export default Select;
