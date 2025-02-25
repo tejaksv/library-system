fa4b5cd7e61153fa034b4f76eda2f24eeeb9ccd5
Tharak1221 <tarakjetipiti@gmail.com>
2025-02-25 17:24:01 +0530

    Added header, footer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import App from './App';
 import HelloWorld from './HelloWorld';
 import reportWebVitals from './reportWebVitals';
 import TableRender from './TableRender';
+import MainCss from './css/main.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TableRender />
+    <header className='mainHeader'>
+      <h1>Company Details</h1>
+    </header>
+      <TableRender />
+    <footer id='mainFooter'>
+      React JS practice
+    </footer>
   </React.StrictMode>
 );
 
