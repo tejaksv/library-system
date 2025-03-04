d38b5f851f0729aeaf9874ab517e75dace0f4297
Varun032001 <bandivarun01@gmail.com>
2025-03-04 17:16:53 +0530

    Select, checkbox, radio components

--- src/TableForm.jsx
+++ src/TableForm.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import Input from './components/Input';
+import Select from './components/select';
+import Checkbox from './components/checkbox';
+import RadioButton from './components/RadioButton'
 
 function TableForm(props) {
     const { company, contact, country, errors, onSave, setCompany, setContact, setCountry } = props;
+    const genderOptions = [{
+        value: 'male',
+        label: 'Male'
+    }, {
+        value: 'female',
+        label: 'Female'
+    }]
+
     return (
         <>
             <Input
@@ -14,7 +25,9 @@ function TableForm(props) {
                 error={errors.company || false}
             />
             <Input title="Contact" id="contact" placeholder="contact name" value={contact} setChangeValue={setContact} error={errors.contact || false} />
-            <Input title="Country" id="country" placeholder="country name" value={country} setChangeValue={setCountry} error={errors.country || false} />
+            <Select title="Country" id="country" setChangeValue={setCountry} />
+            <Checkbox title="VIP" id="vip" defaultValue={true} />
+            <RadioButton title="Gender" name="gender" id="gender" options={genderOptions} />
             <button className='btn btn-primary' onClick={onSave}>Add</button>
         </>
     )
