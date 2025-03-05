b0b207ac508cf6474334f4b1de5a9401a963f670
Tharak1221 <tarakjetipiti@gmail.com>
2025-03-05 11:39:03 +0530

    changes done

--- src/main.js
+++ src/main.js
@@ -3,28 +3,33 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
-const Main = () => {
-  return (
-    <Router>
-      <div className="auth-container">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
-        </nav>
+function Main() {
+  const [page, setPage] = useState('Signup'); 
 
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
+  return (
+      <div className="container text-center mt-4">
+          
+          <div className="mt-3">
+              <button 
+                  className={`btn ${page === 'Login' ? 'btn-primary' : 'btn-outline-primary'} m-2`} 
+                  onClick={() => setPage('Login')}
+              >
+                  Login
+              </button>
+              <button 
+                  className={`btn ${page === 'Signup' ? 'btn-primary' : 'btn-outline-secondary'} m-2`} 
+                  onClick={() => setPage('Signup')}
+              >
+                  Signup
+              </button>
+          </div>
+
+          
+          <div className="mt-4">
+              {page === 'Login' ? <Login /> : <Signup />}
+          </div>
       </div>
-    </Router>
   );
-};
+}
 
 export default Main;
